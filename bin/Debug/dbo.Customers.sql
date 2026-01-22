e8486aae31259d963021d0c3cfe83eadf0c51f93
MaximusFullius <maxfuller5@gmail.com>
2026-01-22 13:32:34 -0600

    dgfyjkgh

---
+++ bin/Debug/dbo.Customers.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustPhone]    CHAR (10)    NOT NULL,
+    [CustName]     VARCHAR (20) NOT NULL,
+    [CustAddress1] VARCHAR (25) NOT NULL,
+    [CustAddress2] VARCHAR (25) NULL,
+    [CustCity]     VARCHAR (20) NOT NULL,
+    [CustState]    CHAR (2)     NOT NULL,
+    [CustZip]      VARCHAR (9)  NOT NULL,
+    [CustOrderNum] INT NOT NULL, 
+    CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([CustOrderNum])
+);
+
